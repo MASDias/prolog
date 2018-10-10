3b8a6f5d41ed73438f1a31580e6b3e4148bd7ba8
1151708 <1151708@isep.ipp.pt>
2018-10-10 18:27:28 +0100

    tp1 ex2 corrigido

--- tp01.pl
+++ tp01.pl
@@ -3,29 +3,15 @@
 :-dynamic(componente/2).
 %1) Carregue para memória a BC definida no ficheiro bicicletas.txt, que contém informação sobre a
 %   especificação dos diferentes componentes de uma bicicleta.
-carregar_ficheiro:-consult("bicicletas.pl").
+load_file:-consult("bicicletas.pl").
 
 %2)gerar_elemento().
 
-novo:-
-                componente(X,Y,_),
-                (
-                    (\+ elemento(X),assertz(elemento(X)));
-                    (\+ elemento(Y),assertz(elemento(Y)))
-                ),
-                fail;true.
-
 gerar_elemento:-
-                findall(X,(componente(X,_,_);componente(_,X,_)),LR),
-                sort(LR, LR2),
-                gerar_elemento(LR2).     
-
-
-gerar_elemento([]).
-
-gerar_elemento([H|T]):-
-                assertz(elemento(H)),
-                gerar_elemento(T).
+                (componente(X,_,_);componente(_,X,_)),
+                \+ elemento(X), 
+                assert(elemento(X)), 
+                fail;true.
                    
 %3)Escreva o predicado produto_final(Elemento) que permite pesquisar elementos que não entram
 %  na composição de outros.
