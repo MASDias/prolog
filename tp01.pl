208c7ab219d09fe782921bab01bc11ca93540d19
Miguel Santos [1161386] <1161386@isep.ipp.pt>
2018-10-11 15:13:39 +0100

    tp01-ex06

--- tp01.pl
+++ tp01.pl
@@ -26,13 +26,26 @@ produto_base(X):-
                 elemento(X),
                 \+componente(X,_,_).
 
-%Escreva o predicado produto_intermedio(Elemento) que permite pesquisar elementos que
+%5)Escreva o predicado produto_intermedio(Elemento) que permite pesquisar elementos que
 %entram na composição de outros e que são formados por outros elementos
 
 produto_intermedio(X):-
                 elemento(X),
                 \+ produto_base(X),
                 \+ produto_final(X).
+
+%6)Escreva o predicado nivel(ElementoX,ElementoY,Nivel) que permite determinar a
+%   profundidade que o ElementoY está na árvore de produto relativamente ao ElementoX,
+%   considere que a raiz está no nível zero.
+nivel(X,Y,N):-diferenca_niveis(Y,X,0,N).
+
+diferenca_niveis(Y,Y,N,N).
+
+diferenca_niveis(Y,W,C,N):-
+                            Y\==W,
+                            componente(W,X,_),
+                            C1 is C+1,
+                            diferenca_niveis(Y,X,C1,N).
 
 
 %8) Escreva o predicado reg_custo(Elemento,Custo)que permite criar um facto (dinâmico) com o custo  de  um  componente,  no  caso de  
