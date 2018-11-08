25da49f6fcdfcc53703f19f2e88d5bcc946e7f9d
1151708 <1151708@isep.ipp.pt>
2018-11-08 17:10:50 +0000

    Algoritmo genetico--Falta fazer criterio de decisao para futuras geracoes

--- TP2/bc_tp2.pl
+++ TP2/bc_tp2.pl
@@ -365,6 +365,17 @@ opt2(Ini,Original,[[A,B]|T1],[[C,D]|T],Result):-
 				).
 
 
+
+
+%%-----------------------------------------------------------------------------------------------------------------
+%%--------------------------------------------ITERACAO 4 - GENETICOS--------------------------------------------%%
+custoCaminho(L,Custo):-
+    L=[Ini|T],
+    custo(L,Custo1),
+    append(_,[Ultimo],T),
+    dist_cities(Ultimo,Ini,CustoVoltaOrigem),
+    Custo is Custo1+CustoVoltaOrigem.
+
 custo([_],0):-!.
 
 custo([A,B|T], Custo):-
@@ -377,30 +388,27 @@ geracoes(5).
 populacao(4).
 prob_cruzamento(0.4).
 prob_mutacao(0.5).
-cities(6).
+%cities(6).
 
 
+gerarNumeroCidades:-
+	retractall(cities(_)),
+    findall(Cidade,city(Cidade,_,_),ListaCidades),
+    length(ListaCidades,N),
+    assert(cities(N))
 gera:-
-	% Pop = [Origem],
-	gerar_populacao(Pop).
-	
-	% avalia_populacao(Pop,PopAv),
-	% ordena_populacao(PopAv,PopOrd),
-	% geracoes(NG),
-	% gera_geracao(NG,PopOrd)
+	gerarNumeroCidades,
+	gerar_populacao(Pop),
+	avalia_populacao(Pop,PopAv), % Avaliacao do custo dos individuos(lista de possiveis caminhos) da populacao
+	ordena_populacao(PopAv,PopOrd), % Apos a avaliacao da populacao faz uma ordenacao à mesma
+	geracoes(NG),
+	gera_geracao(NG,PopOrd).
 
 gerar_populacao(Pop):-
 	populacao(TamanhoPopulacao),
 	cities(NumCidades),
 	findall(Cidade,city(Cidade,_,_), ListaCidades),
-	gera_populacao(TamanhoPopulacao,ListaCidades,NumCidades,Pop_sem_custo),
-	gerar_populacao_custo(Pop_sem_custo,Pop).
-
-gerar_populacao_custo([],_):-!.
-gerar_populacao_custo([H|T],Pop):-
-	custo(H,Custo),
-	gerar_populacao_custo(T,Pop),
-	append(List1, List2, List1AndList2).
+	gera_populacao(TamanhoPopulacao,ListaCidades,NumCidades,Pop).
 
 gera_populacao(0,_,_,[]):-!.
 
@@ -428,3 +436,174 @@ retira(N,[G1|Resto],G,[G1|Resto1]):-
 	N1 is N-1,
 	retira(N1,Resto,G,Resto1).
 
+avalia_populacao([],[]).
+avalia_populacao([Ind|Resto],[Ind*V|Resto1]):-
+	avalia(Ind,V),
+	avalia_populacao(Resto,Resto1).
+
+avalia(Cidades,Custo):-
+	custoCaminho(Cidades,Custo).
+
+ordena_populacao(PopAv,PopOrd):-
+	bSort(PopAv,PopOrd).
+
+bSort([X],[X]):-!.
+bSort([X|Xs],Ys):-
+	bSort(Xs,Zs),
+	btroca([X|Zs],Ys).
+
+btroca([X],[X]):-!.
+
+btroca([X*VX,Y*VY|L1],[Y*VY|L2]):-
+	VX>VY,!,
+	btroca([X*VX|L1],L2).
+
+btroca([X|L1],[X|L2]):-btroca(L1,L2).
+
+gera_geracao(0,Pop):-!,
+	write('Gera��o '), write(0), write(':'), nl, write(Pop), nl.
+
+gera_geracao(G,Pop):-
+	write('Gera��o '), write(G), write(':'), nl, write(Pop), nl,
+	cruzamento(Pop,NovaPopulacao1),
+	mutacao(NovaPopulacao1,NovaPopulacao),
+	avalia_populacao(NovaPopulacao,NovaPopulacaoAvaliada),
+	ordena_populacao(NovaPopulacaoAvaliada,NovaPopulacaoOrd),
+	Geracoes is G-1,
+	gera_geracao(Geracoes,NovaPopulacaoOrd).
+
+
+
+cruzamento([],[]).
+cruzamento([Ind*_],[Ind]).
+cruzamento([Ind1*_,Ind2*_|Resto],[NInd1,NInd2,Ind1,Ind2|Resto1]):-
+	gerar_pontos_cruzamento(P1,P2),
+	prob_cruzamento(ProbCruzamento),
+	random(0.0,1.0,Pc),
+	(
+		(
+		Pc =< ProbCruzamento,!,
+        cruzar(Ind1,Ind2,P1,P2,NInd1),
+		cruzar(Ind2,Ind1,P1,P2,NInd2)
+		)
+	;
+		(
+		NInd1=Ind1,NInd2=Ind2
+		)
+	),
+	cruzamento(Resto,Resto1).
+
+gerar_pontos_cruzamento(P1,P2):-
+	gerar_pontos_cruzamento1(P1,P2).
+
+gerar_pontos_cruzamento1(P1,P2):-
+	cities(N),
+	NTemp is N+1,
+	random(1,NTemp,P11),
+	random(1,NTemp,P21),
+	P11\==P21,!,
+	((P11<P21,!,P1=P11,P2=P21);P1=P21,P2=P11).
+gerar_pontos_cruzamento1(P1,P2):-
+	gerar_pontos_cruzamento1(P1,P2).
+
+preencheh([],[]).
+
+preencheh([_|R1],[h|R2]):-
+	preencheh(R1,R2).
+
+
+sublista(L1,I1,I2,L):-
+	I1 < I2,!,
+	sublista1(L1,I1,I2,L).
+
+sublista(L1,I1,I2,L):-
+	sublista1(L1,I2,I1,L).
+
+sublista1([X|R1],1,1,[X|H]):-!,
+	preencheh(R1,H).
+
+sublista1([X|R1],1,N2,[X|R2]):-!,
+	N3 is N2 - 1,
+	sublista1(R1,1,N3,R2).
+
+sublista1([_|R1],N1,N2,[h|R2]):-
+	N3 is N1 - 1,
+	N4 is N2 - 1,
+	sublista1(R1,N3,N4,R2).
+
+rotate_right(L,K,L1):-
+	cities(N),
+	T is N - K,
+	rr(T,L,L1).
+
+rr(0,L,L):-!.
+
+rr(N,[X|R],R2):-
+	N1 is N - 1,
+	append(R,[X],R1),
+	rr(N1,R1,R2).
+
+
+elimina([],_,[]):-!.
+
+elimina([X|R1],L,[X|R2]):-
+	not(member(X,L)),!,
+	elimina(R1,L,R2).
+
+elimina([_|R1],L,R2):-
+	elimina(R1,L,R2).
+
+insere([],L,_,L):-!.
+insere([X|R],L,N,L2):-
+	cities(T),
+	((N>T,!,N1 is N mod T);N1 = N),
+	insere1(X,N1,L,L1),
+	N2 is N + 1,
+	insere(R,L1,N2,L2).
+
+
+insere1(X,1,L,[X|L]):-!.
+insere1(X,N,[Y|L],[Y|L1]):-
+	N1 is N-1,
+	insere1(X,N1,L,L1).
+
+cruzar(Ind1,Ind2,P1,P2,NInd11):-
+	sublista(Ind1,P1,P2,Sub1),
+	cities(NumeroCidades),
+	R is NumeroCidades-P2,
+	rotate_right(Ind2,R,Ind21),
+	elimina(Ind21,Sub1,Sub2),
+	P3 is P2 + 1,
+	insere(Sub2,Sub1,P3,NInd1),
+	eliminah(NInd1,NInd11).
+
+eliminah([],[]).
+
+eliminah([h|R1],R2):-!,
+	eliminah(R1,R2).
+
+eliminah([X|R1],[X|R2]):-
+	eliminah(R1,R2).
+
+mutacao([],[]).
+mutacao([Ind|Rest],[NInd|Rest1]):-
+	prob_mutacao(Pmut),
+	random(0.0,1.0,Pm),
+	((Pm < Pmut,!,mutacao1(Ind,NInd));NInd = Ind),
+	mutacao(Rest,Rest1).
+
+mutacao1(Ind,NInd):-
+	gerar_pontos_cruzamento(P1,P2),
+	mutacao22(Ind,P1,P2,NInd).
+
+mutacao22([G1|Ind],1,P2,[G2|NInd]):-
+	!, P21 is P2-1,
+	mutacao23(G1,P21,Ind,G2,NInd).
+mutacao22([G|Ind],P1,P2,[G|NInd]):-
+	P11 is P1-1, P21 is P2-1,
+	mutacao22(Ind,P11,P21,NInd).
+
+mutacao23(G1,1,[G2|Ind],G2,[G1|Ind]):-!.
+mutacao23(G1,P,[G|Ind],G2,[G|NInd]):-
+	P1 is P-1,
+	mutacao23(G1,P1,Ind,G2,NInd).
